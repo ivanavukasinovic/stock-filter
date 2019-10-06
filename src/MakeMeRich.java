aec299824e0f80deae1913c9f6c015a1490e40ed
Your Name <you@example.com>
2019-10-06 14:26:21 +0200

    Task completed

--- src/MakeMeRich.java
+++ src/MakeMeRich.java
@@ -1,6 +1,8 @@
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MakeMeRich {
 	public static final List<String> symbols = Arrays.asList("AMD", "HPQ",
@@ -11,12 +13,28 @@ public class MakeMeRich {
 	public static void main(String[] args) {
 
 		// 1. Print these symbols using a Java 8 for-each and lambdas
+		symbols.stream().forEach(System.out::println);
+		System.out.println();
 
 		// 2. Use the StockUtil class to print the price of Bitcoin
+		System.out.println("The price of Bitcoin: " + StockUtil.getPrice("BTC-USD"));
+		System.out.println();
 
 		// 3. Create a new List of StockInfo that includes the stock price
+		List<StockInfo> listStockInfo = StockUtil.prices.entrySet()
+				.stream()
+				.map(stock -> new StockInfo(stock.getKey(), stock.getValue()))
+				.collect(Collectors.toList());
+		listStockInfo.forEach(System.out::println);
+		System.out.println();
 	
 		// 4. Find the highest-priced stock under $500
+		StockInfo highestPricedStock = listStockInfo
+				.stream()
+				.filter(StockUtil.isPriceLessThan(500))
+				.reduce(StockUtil::pickHigh)
+				.get();
+		System.out.println("The highest-priced stock under $500 is: " + highestPricedStock);
 	
 	}
 
